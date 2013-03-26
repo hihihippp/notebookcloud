7d2d38cab2961a6afb71eb697f4b7600d6f87571
Will Welch <welch@quietplease.com>
2013-03-25 18:11:27 -0700

    add ec2_key attribute to account object, and propagate this to ec2 instances at creation so ssh/scp are possible.
    modify funcs.py to operate with latest Boto version, which
    changed its is_secure and validate_certificates defaults to True
    and cause failures within GAE.
    
    modify get_instance_list to use instance accessors instad of
    __dict__, because inst.state has become a @property in the
    current boto and no longer appears in __dict__.
    
    split login page into ask_login and create_account
    pages so we can include a sign-out button in the workflow.

--- funcs.py
+++ funcs.py
@@ -14,15 +14,30 @@ from google.appengine.ext.webapp import template
 import boto
 from boto.ec2.connection import EC2Connection
 
+#
+# work around broken certificate validation in app engine, for now.
+# This also requires that you call EC2Connection(... is_secure=False)
+#
+if not boto.config.has_section('Boto'):
+    boto.config.add_section('Boto')
+boto.config.set('Boto', 'https_validate_certificates', 'False')
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates/')
 
 
 def valid_keys(access_key, secret_key):
     
-    try: EC2Connection(access_key, secret_key).get_all_instances()       
+    try: EC2Connection(access_key, secret_key, is_secure=False)\
+            .get_all_instances()       
     except: return False
     return True
 
+def valid_ec2_key(access_key, secret_key, key_name):
+    
+    try: EC2Connection(access_key, secret_key, is_secure=False)\
+            .get_all_key_pairs([key_name])[0]       
+    except: return False
+    return True
 
 def hash_password(password):
     
@@ -49,23 +64,24 @@ def get_instance_list(access_key, secret_key):
     html_output = ''
     refresh = False
     
-    connection = EC2Connection(access_key, secret_key)
+    connection = EC2Connection(access_key, secret_key, is_secure=False)
     
     reservations = connection.get_all_instances()
     instances = [inst for res in reservations for inst in res.instances]
 
     for inst in instances:
         
-        ours  = inst.__dict__['image_id'] == 'ami-affe51c6'
-        lives = inst.__dict__['state'] != 'terminated'
+        ours  = inst.image_id == 'ami-affe51c6'
+        lives = (inst.state != 'terminated')
         
         if ours and lives:
 
-            dns_name      = inst.__dict__['public_dns_name']
-            state         = inst.__dict__['state']
-            instance_type = inst.__dict__['instance_type']
-            instance_id   = inst.__dict__['id']
-            date, time    = inst.__dict__['launch_time'].split('T')
+            dns_name      = inst.public_dns_name
+            state         = inst.state
+            instance_type = inst.instance_type 
+            instance_id   = inst.id 
+            date, time    = inst.launch_time.split('T')
+            key_name      = inst.key_name
         
             time=time[:-5]
 
@@ -78,13 +94,19 @@ def get_instance_list(access_key, secret_key):
             # servers are classed as booting or serving
             if state == 'running': state = 'booting'
             
-            html_string = (
+            html_output += (
                 '<div class=instance>Instance id: <b>{}</b>Type: <b>{}</b>'
-                'Started: <b>{}</b> ~ <b>{}</b><br>{}State: '
-                ).format(instance_id + tab, instance_type + tab, time, date, tab)
+                'Started: <b>{}</b> ~ <b>{}</b><br>'
+                ).format(instance_id + tab, instance_type + tab, time, date)
         
             if dns_name:
             
+                if key_name is not None:
+                    
+                    html_output += ( tab +
+                        'ssh: <span id="serving">ubuntu@{} (key={})</span><br>'
+                        ).format(dns_name, key_name)
+
                 try: # check if the instance is actively serving
             
                     urlfetch.fetch(
@@ -96,7 +118,7 @@ def get_instance_list(access_key, secret_key):
                 except:
             
                     state = '<b>'+state+'</b>'
-                    html_output += html_string + state + '</div>'
+                    html_output += tab + "State: "+ state
                 
                 else:
             
@@ -105,9 +127,9 @@ def get_instance_list(access_key, secret_key):
                     serving_buttons = template.render(html, args)
                 
                     html_output += (
-                        html_string +
-                        '<a id="serving" href="https://{}:8888">serving</a>{}</div>'
-                        ).format(dns_name, serving_buttons)
+                        '{}Serving at <a id="serving" '
+                        'href="https://{}:8888">{}:8888</a>{}'
+                        ).format(tab, dns_name, dns_name, serving_buttons)
                 
                     # now we know we're running, not booting
                     transitional = False
@@ -120,12 +142,14 @@ def get_instance_list(access_key, secret_key):
                     html = template_dir + 'stopped_buttons.html'
                     args = {'instance': instance_id}
                     stopped_buttons = template.render(html, args)
-                    html_output += html_string + state + stopped_buttons + '</div>'
+                    html_output += tab + 'State: ' + state + stopped_buttons
             
                 else:
             
                     state = '<b>'+state+'</b>'
-                    html_output += html_string + state + '</div>'
+                    html_output += tab + 'State: ' + state
+
+            html_output += '</div>'
         
             if transitional: refresh = True
     
@@ -140,9 +164,9 @@ def get_instance_list(access_key, secret_key):
     return refresh, html_output
 
 
-def create_vm(access_key, secret_key, user_details, instance_class):
+def create_vm(access_key, secret_key, user_details, instance_class, ec2_key):
 
-    connection = EC2Connection(access_key, secret_key)
+    connection = EC2Connection(access_key, secret_key, is_secure=False)
 
     group_name  = 'notebookcloud_group'
     description = 'NotebookCloud: Default Security Group.'
@@ -150,11 +174,23 @@ def create_vm(access_key, secret_key, user_details, instance_class):
     try: group = connection.create_security_group(group_name, description)
     except: pass
     else: group.authorize('tcp', 8888,8888, '0.0.0.0/0')
+    security_groups = ['notebookcloud_group']
+
+    if ec2_key:
+
+        group_name  = 'notebookcloud_ssh_group'
+        description = 'NotebookCloud: SSH Security Group.'
+    
+        try: group = connection.create_security_group(group_name, description)
+        except: pass
+        else: group.authorize('tcp', 22, 22, '0.0.0.0/0')
+        security_groups.append('notebookcloud_ssh_group')
 
     reservation = connection.run_instances(
         'ami-affe51c6',
         instance_type=instance_class,
-        security_groups=['notebookcloud_group'],
+        security_groups=security_groups,
+        key_name=ec2_key,
         user_data=user_details,
         max_count=1
         )
@@ -164,7 +200,7 @@ def create_vm(access_key, secret_key, user_details, instance_class):
     
 def control_vm(action, instance_list, access_key, secret_key):
     
-    connection = EC2Connection(access_key, secret_key)
+    connection = EC2Connection(access_key, secret_key, is_secure=False)
     
     if action == 'terminate':
         connection.terminate_instances(instance_ids=instance_list)
